de9458b51551a4a229a4d7877e511602dcef4a78
DESKTOP-AVQ3BDH\Alex <alexander@playbuzz.com>
2018-11-08 10:27:47 +0200

    WIP

--- TestProjectSB/src/test/UdemySanity.java
+++ TestProjectSB/src/test/UdemySanity.java
@@ -4,12 +4,31 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class UdemySanity {
 	
+	private final String USERNAME = "alexander@playbuzz.com";
+	private final String PASSWORD = "Aa123456";
+	
   @Test
   public void testSeleniumCourses() {
+	  UdemyBasePage basePage = new UdemyBasePage(driver);
+	  UdemyLoginPage login = homePage.goToLogin();
+	  UdemyHomePage homePage = login.performLogin(USERNAME, PASSWORD);
+	  UdemyPopularCoursesPage popularCourses =  homePage.headerSection.selectCategory(category).selectSubCategory(subCategory);
+	  UdemyRelevantCoursesPage relevantCoursesPage = popularCourses.headerSection.searchForCourserWithSearchBox(topic);
+	  relevantCourses.openFilters().choosePriceFilter(PriceOptions.FREE);
+	  List<UdemyCourse> coursesCollection = relevantCourses.coursesCollection;
+	  Assert.assertTrue(coursesCollection.length >= minimumNumberOfCourses);
+	  bool keyWordExists = false;
+	  for(UdemyCourse course : coursesCollection) {
+		  Assert.assertTrue(course.isFree());
+		  if(course.getTitle().contains(keyWord))
+			  keyWordExists = true;
+	  }
+	  Assert.assertTrue(keyWordExists);
   }
   
   @BeforeMethod
