03f2f964d2daa8d4082923e22d9aeef1d3fd61f1
DESKTOP-AVQ3BDH\Alex <alexander@playbuzz.com>
2018-11-08 23:26:22 +0200

    WIP

---
+++ TestProjectSB/src/pageObject/UdemyBasePage.java
@@ -0,0 +1,13 @@
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class UdemyBasePage {
+	
+	private WebDriver driver;
+
+	public UdemyBasePage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
--- TestProjectSB/src/test/UdemySanity.java
+++ TestProjectSB/src/test/UdemySanity.java
@@ -1,9 +1,22 @@
 package test;
 
 import org.testng.annotations.Test;
+
+import pageObject.UdemyBasePage;
+import pageObject.UdemyCourseSection;
+import pageObject.UdemyHomePage;
+import pageObject.UdemyLoginPage;
+import pageObject.UdemyPopularCoursesPage;
+import pageObject.UdemyRelevantCoursesPage;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
@@ -12,20 +25,22 @@ public class UdemySanity {
 	private final String USERNAME = "alexander@playbuzz.com";
 	private final String PASSWORD = "Aa123456";
 	
+	private WebDriver driver;
+	
   @Test
   public void testSeleniumCourses() {
 	  UdemyBasePage basePage = new UdemyBasePage(driver);
-	  UdemyLoginPage login = homePage.goToLogin();
+	  UdemyLoginPage login = basePage.goToLogin();
 	  UdemyHomePage homePage = login.performLogin(USERNAME, PASSWORD);
 	  UdemyPopularCoursesPage popularCourses =  homePage.headerSection.selectCategory(category).selectSubCategory(subCategory);
 	  UdemyRelevantCoursesPage relevantCoursesPage = popularCourses.headerSection.searchForCourserWithSearchBox(topic);
-	  relevantCourses.openFilters().choosePriceFilter(PriceOptions.FREE);
-	  List<UdemyCourse> coursesCollection = relevantCourses.coursesCollection;
+	  relevantCoursesPage.openFilters().choosePriceFilter(PriceOptions.FREE);
+	  List<UdemyCourseSection> coursesCollection = relevantCourses.coursesSectionCollection;
 	  Assert.assertTrue(coursesCollection.length >= minimumNumberOfCourses);
-	  bool keyWordExists = false;
-	  for(UdemyCourse course : coursesCollection) {
+	  Boolean keyWordExists = false;
+	  for(UdemyCourseSection course : coursesCollection) {
 		  Assert.assertTrue(course.isFree());
-		  if(course.getTitle().contains(keyWord))
+		  if(!keyWordExists && course.getTitle().contains(keyWord))
 			  keyWordExists = true;
 	  }
 	  Assert.assertTrue(keyWordExists);
@@ -33,10 +48,12 @@ public class UdemySanity {
   
   @BeforeMethod
   public void beforeMethod() {
+	  driver = new ChromeDriver();
   }
 
   @AfterMethod
   public void afterMethod() {
+	  driver.close();
   }
 
   @BeforeClass
